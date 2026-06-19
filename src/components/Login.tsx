6400d7b53fd90505c65113de649d05a0439af6a7
Samar Maharjan <samarmhrzn257@gmail.com>
2026-06-20 00:36:08 +0545

    Home page Ui and routing

---
+++ src/components/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='flex justify-center items-center h-screen  '>
+      <div className='w-100 h-96 bg-white rounded-xl shadow-md'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/Pricing.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Pricing() {
+  return (
+    <div>
+      this is pricing $20
+    </div>
+  )
+}
+
+export default Pricing
